3851b1f74fab149b8c4328a0a7f05eacd5f561b6
Anish Pednekar <pednekar.anish@gmail.com>
2014-07-24 09:23:26 +0530

    Initial Commit

---
+++ openGL_GLUT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openGL_GLUT
+//
+//  Created by Anish Pednekar on 7/24/14.
+//  Copyright (c) 2014 Anish Pednekar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
